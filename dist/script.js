21af613f3680db8e38ea1a7993e99bb5ce13b082
Mike <mwroberts89@gmail.com>
2020-11-02 17:14:01 -0500

    the raw search function is coming together, though still very basic

--- dist/script.js
+++ dist/script.js
@@ -10,11 +10,25 @@ const fileUpload = document.querySelector("#file-upload"),
 fileNameDisplay = document.querySelectorAll(".file-name");
 
 const initialSearchBtn = document.getElementById("initial-search-button"),
-initialSearchTerm = document.getElementById("initial-search-term");
+caseSensitivity = document.getElementById('case-sensitive');
 
-const documentDisplay = document.querySelector(".document-display");
+let initialSearchTerm = document.getElementById("initial-search-term");
+
+const documentDisplay = document.getElementById("document-display"),
+documentContent = document.querySelector(".document-content"),
+searchStats = document.querySelector(".search-stats"),
+searchedWord = document.querySelector(".searched-word"),
+newSearchTerm = document.getElementById('new-search'),
+newSearchBtn = document.querySelector(".new-search-btn");
+
+const docsLink = document.getElementById('docs-link'),
+dlDocs = document.getElementById('dl-docs');
+
+let sessionDoc = '';
 
 // Event listeners
+docsLink.addEventListener('click', () => dlDocs.style.display = 'block');
+
 fileUpload.addEventListener("change", () => {
     section2.classList.add("show");
 
@@ -26,32 +40,94 @@ fileUpload.addEventListener("change", () => {
 useTemplateBtn.addEventListener('click', () => section3a.classList.add("show"));
 
 noTemplateBtn.addEventListener('click', () => section3b.classList.add("show"));
+        // let searchedDoc = doc.split('LABEL');
 
 initialSearchBtn.addEventListener("click", () => {
-    documentDisplay.style.display = 'block';
-    console.log(documentDisplay);
-    loadDocx(fileUpload.files[0]);
+    // Make sure search term is entered
+    if (!initialSearchTerm.value) {
+        window.alert('please enter search term');
+    } else {
+        initialSearchTerm = initialSearchTerm.value;
+        documentDisplay.style.display = 'block';
+        documentContent.textContent = 'loading...';
+        loadDocx(fileUpload.files[0]);
+    }
 });
+
+newSearchBtn.addEventListener("click", () => {
+    rawSearchDoc(sessionDoc, newSearchTerm.value);
+})
 
 // Functions
 function loadDocx(file) {
     const reader = new FileReader();
     reader.readAsArrayBuffer(file);
-    console.log(reader.readyState);
+    // console.log(reader.readyState);
 
-    setTimeout(()=>{
-        console.log(reader.readyState);
-        console.log(reader.result);
-        let arrayBuffer = reader.result;
-
-        mammoth.convertToHtml({arrayBuffer:arrayBuffer}).then((result) => {
-            documentDisplay.innerHTML = result.value;
-            console.log(result.value);
-        });
-
-        mammoth.extractRawText({arrayBuffer:arrayBuffer}).then((result) => {
-            console.log(result.value)
-        });
-    },1000);
+    // Make sure reader is ready before displaying
+    const readyStateCheck = setInterval(() => {
+        if (reader.readyState === 2) {
+            let arrayBuffer = reader.result;
+    
+            mammoth.convertToHtml({arrayBuffer:arrayBuffer}).then((result) => {
+                sessionDoc = result.value;
+                rawSearchDoc(result.value, initialSearchTerm);
+            });
+    
+            mammoth.extractRawText({arrayBuffer:arrayBuffer}).then((result) => {
+                // console.log(result.value)
+            });
+            
+            clearInterval(readyStateCheck);
+        }            
+    }, 100)
 }
 
+function rawSearchDoc(doc, searchTerm) {
+    let caseCheck;
+
+    let regMatch = 0,
+    exactMatch = 0;
+
+    if(doc.search(searchTerm.toLowerCase()) === -1 && doc.search(searchTerm.toUpperCase()) === -1){
+        window.alert('term not found in document');
+    } else {
+        console.log(searchTerm.length);
+        console.log(doc.length);
+
+        // counts the number of exact matches and regular matches
+        for(let i = 0; i < doc.length; i++) {
+            if (doc.substring(i, searchTerm.length + i) === searchTerm) {
+                exactMatch++;              
+            } 
+
+            if (doc.substring(i, searchTerm.length + i).toLowerCase() === searchTerm.toLowerCase()) {
+                regMatch++;
+            }
+        }
+
+        displaySearchStats(exactMatch, regMatch, searchTerm);
+
+        let termHighlight = `<span style="color: red; font-weight: bold">${searchTerm}</span>`;
+        
+        if (caseSensitivity.checked) {
+            caseCheck = "g";
+        } else {
+            caseCheck = "gi";
+        }
+
+        let regex = RegExp(searchTerm, caseCheck);
+        doc = doc.replace(regex, termHighlight);
+
+        documentContent.innerHTML = doc;
+    };
+}
+
+function displaySearchStats(exactMatch, regMatch, searchTerm) {
+    searchedWord.innerHTML = `'${searchTerm}'`;
+
+    searchStats.innerHTML = `
+    <div><strong>Match:</strong>${regMatch}</div>
+        <div><strong>Exact Match:</strong>${exactMatch}</div>
+    `;
+}
